e4537b9a7ced9bd592597b469d06ad814c9cc078
Sean Suzuki <suzatsuhiro@gmail.com>
2023-01-14 18:57:39 -0800

    First commit

---
+++ bin/Debug/net7.0/browserextension/BackgroundWorker.js
@@ -0,0 +1,45 @@
+﻿// Import for the side effect of defining a global 'browser' variable
+import * as _ from "/content/Blazor.BrowserExtension/lib/browser-polyfill.min.js";
+
+chrome.runtime.onMessage.addListener(
+  function(msg, sender, onSuccess) {
+      var arg, cmd;
+      [cmd, arg] = msg;
+
+      if (cmd == "GetSubscriptionDate") {
+        GetSubscriptionDateV2(arg, onSuccess);
+      }
+      else if(cmd == "GetChannelID") {
+        GetChannelID(arg, onSuccess);
+      }
+      else if(cmd == "TestGoogleOAuth") {
+        GetChannelIDV2(arg, onSuccess)
+      }
+      return true;  // Will respond asynchronously.
+  }
+);
+
+function GetChannelID(url, onSuccess) {
+  fetch(url, {
+    headers: {
+        'referer': 'https://www.youtube.com/'
+    }
+  })
+  .then(response => response.text())
+  .then(responseText => onSuccess(responseText))
+}
+
+function GetSubscriptionDateV2(url, onSuccess) {
+  console.log("Testing TestGoogleOAuth");
+  chrome.identity.getAuthToken({interactive: true}, function(token) {
+    console.log(token);
+    fetch(url, {
+      headers: {
+          'referer': 'https://www.youtube.com/',
+          'Authorization': 'Bearer ' + token
+      }
+    })
+    .then(response => response.text())
+    .then(responseText => onSuccess(responseText))
+  });
+}
---
+++ bin/Debug/net7.0/wwwroot/content.js
@@ -0,0 +1,77 @@
+function GetChannelId() {
+    chrome.runtime.sendMessage( //goes to bg_page.js
+        ["GetChannelID", location.href],
+        data => {
+            let result = data.match("\"channelId\"\:\"([^\"]*)\"");
+            var channelId = result[1];
+
+            SetSubscriptionDate(channelId)
+        }
+    )
+}
+
+function GetSubscriptionInfoUrl(channelId) {
+    return `https://content.googleapis.com/youtube/v3/subscriptions?mine=true&maxResults=50&part=snippet&key=AIzaSyAlp4lZkYS1x4jT4oJ3lMj_-GpPZcd1Grg&forChannelId=${channelId}`
+}
+
+function IsYoutubeVideoPage() {
+    return location.href.indexOf("https://www.youtube.com/watch?v=") >= 0
+}
+
+function RetrieveSubscriptionDate() {
+    if (!IsYoutubeVideoPage()) return;
+
+    GetChannelId();
+}
+
+function SetSubscriptionDate(channelId) {
+    let subscriberInfoUrl = GetSubscriptionInfoUrl(channelId);
+    chrome.runtime.sendMessage( //goes to bg_page.js
+        ["GetSubscriptionDate", subscriberInfoUrl],
+        data => {
+            let responseBlob = JSON.parse(data);
+            date = responseBlob["items"][0]["snippet"]["publishedAt"];
+            
+            let dateTime = new Date(date).toLocaleDateString();
+            SetSubscriptionText(dateTime);
+        }
+    )
+}
+
+function SetSubscriptionText(datetime) {
+    let subscriptionParentClass = document.body;
+
+    let observer = new MutationObserver(function(mutations){
+
+        let subscriptionButton = document.getElementById("notification-preference-button");
+
+        if(subscriptionButton) {
+            let subscriptionText = subscriptionButton.getElementsByClassName("yt-core-attributed-string yt-core-attributed-string--white-space-no-wrap")[0];
+
+            if (subscriptionText.textContent == "Subscribed") {
+                subscriptionText.textContent = `${subscriptionText.textContent} since ${datetime}`;
+                observer.disconnect(); // to stop observing the dom
+            }
+        }
+    })
+
+    observer.observe(subscriptionParentClass, { 
+        childList: true,
+        subtree: true // needed if the node you're targeting is not the direct parent
+    });
+}
+
+RetrieveSubscriptionDate();
+
+let lastUrl = location.href; 
+new MutationObserver(() => {
+  const url = location.href;
+  if (url !== lastUrl) {
+    lastUrl = url;
+    onUrlChange();
+  }
+}).observe(document, {subtree: true, childList: true});
+
+function onUrlChange() {
+  RetrieveSubscriptionDate();
+}
